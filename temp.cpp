23c306942e5084071a4457dc81a619b52bd8d36a
Lntano-S <1932474243@qq.com>
2026-01-28 15:16:46 +0800

    Update

--- temp.cpp
+++ temp.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
 
-int main() {
+int main(void) {
+    int x;
+    std::cin >> x;
+
+    bool b1 = x % 2 == 0;
+    bool b2 = (4 < x) && (x <= 12);
+
+    if (b1 && b2) {
+        std::cout << "1 ";
+    } else {
+        std::cout << "0 ";
+    }
+
+    if (b1 || b2) {
+        std::cout << "1 ";
+    } else {
+        std::cout << "0 ";
+    }
+
+    if ((b1 && !b2) || (!b1 && b2))  {
+        std::cout << "1 ";
+    } else {
+        std::cout << "0 ";
+    }
+
+    if (!b1 && !b2) {
+        std::cout << "1 ";
+    } else {
+        std::cout << "0 ";
+    }
+
     return 0;
 }
