15c72e66e10161e0df581e33763bffffa6abc3c2
Lntano-S <1932474243@qq.com>
2026-03-12 08:59:28 +0800

    Update

--- pointer_函数指针.cpp
+++ pointer_函数指针.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
+/* 
+定义一个指针函数，返回类型：布尔值
+               *：表示这是一个指针
+               int：表示输入值的类型
+
+*/
 using ConditionFunc = bool (*)(int);
 
+// 数组过滤函数
 std::vector<int> filterArray(const std::vector<int>& arr, ConditionFunc condition) {
     std::vector<int> result;
+
     for (int num : arr) {
+        // 利用指针函数返回的布尔值进行 if 条件判断
         if (condition(num)) {
             result.push_back(num);
         }
@@ -25,8 +35,13 @@ int main(void) {
         return m > 10;
     });
 
-    std::sort
+    std::sort(numbers.begin(), numbers.end(), [](int a, int b) -> bool {
+        return a > b;
+    });
 
+    for (int num : numbers) {
+        std::cout << num << std::endl;
+    }
 
     return 0;
 }
